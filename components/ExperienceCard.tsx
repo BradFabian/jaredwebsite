7f66a6e251e622bfa75fc580979765710047c265
Brad Fabian <bradleofabian@gmail.com>
2022-12-27 22:50:35 -0500

    added experience

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import { motion } from 'framer-motion';
 type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <div>ExperienceCard</div>
+    <article>
+        <motion.img 
+        initial= {{
+            y:-100,
+            opacity: 0,
+        }}
+        transition={{duration: 1.2}}
+        whileInView={{ opacity: 1, y: 0}}
+        viewport={{once: true}}
+        
+        className="max-w-sm xl:h-[200px] xl:w-[200px] object-cover object-center" src="https://www.swiftcentre.org/content/images/2022/06/Untitled-10-1.png" />
+        <div>
+            <h4>Forecaster</h4>
+            <p>Swift Centre for Applied Forecasting</p>
+        </div>
+
+    </article>
   )
 }
 
