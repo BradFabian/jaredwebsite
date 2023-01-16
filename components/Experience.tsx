0c179536a9f6687d6258de243409cc25de88114d
Brad Fabian <bradleofabian@gmail.com>
2023-01-16 10:51:36 -0500

    added styling for experience and accoldes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -22,7 +22,7 @@ export default function Experience({}: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
        
         
-        <div>
+        <div className="">
             < ExperienceCard />
             < ExperienceCard />
             < ExperienceCard />
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <article>
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px]" >
         <motion.img 
         initial= {{
             y:-100,
@@ -14,10 +14,15 @@ function ExperienceCard({}: Props) {
         whileInView={{ opacity: 1, y: 0}}
         viewport={{once: true}}
         
-        className="max-w-sm xl:h-[200px] xl:w-[200px] object-cover object-center" src="https://www.swiftcentre.org/content/images/2022/06/Untitled-10-1.png" />
+        className="max-w-sm xl:h-[50px] xl:w-[320px] object-cover object-center" src="https://www.swiftcentre.org/content/images/2022/06/Untitled-10-1.png" />
         <div>
             <h4>Forecaster</h4>
             <p>Swift Centre for Applied Forecasting</p>
+
+            <ul clasName="list-disc space-y-4 ml-5 text-lg">
+              <li>Deliver forecasting services for publication in various media outlets</li>
+              <li>Details available here: <a href="https://www.swiftcentre.org" rel="noreferrer" target="_blank">https://www.swiftcentre.org/</a></li>
+            </ul>
         </div>
 
     </article>
